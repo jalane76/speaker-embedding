38311e4f8a3f0b2584b31d2e2ad3ff580c522b1e
Jesse Alane Lane - student <jalane@scslab01.me.iastate.edu>
2020-10-17 08:43:16 -0500

    Moved training scripts to training directories to copy them to results.

--- cmumosei-training-database/train-cmu-mosei.py
+++ cmumosei-training-database/train-cmu-mosei.py
@@ -10,7 +10,16 @@ def train():
     protocol = 'CMUMOSEI.SpeakerDiarization.CMUMOSEI'
     model_path = '/data/speaker-embeddings/models/voxceleb-pretrained/0560.pt'
 
-    subprocess.run([script, training_db_path, output_path, protocol, '--model_path', model_path])
+    commands = [
+        script,
+        training_db_path,
+        output_path,
+        protocol,
+        #'--model_path', model_path,
+        '--epochs', '1000'
+    ]
+
+    subprocess.run(commands)
 
 if __name__ == '__main__':
     train()
